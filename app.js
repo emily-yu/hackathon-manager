694d171b68a0c4abb8fd14ee88788362aa60acbf
ey1031 <eyudeveloper@gmail.com>
2017-10-28 02:27:53 -0700

    adding base files for other tabs

---
+++ app.js
@@ -0,0 +1,28 @@
+function loadData() {
+	function csvToArray (csv) {
+	    rows  = csv.split("\n");
+	    return rows.map(function (row) {
+	    	return row.split(",");
+	    });
+	};
+
+	// Hard-coded for brevity, but you can set this variable with FileReader
+	var csv = CSVParse("data.csv")
+
+	var array = csvToArray(csv);
+	return array;
+}
+
+function createTable() {
+    var array = [["god bless",2,3,4],[4,5,6,4],[7,8,9,4]];
+    // var array = loadData();
+    var content = "";
+    array.forEach(function(row) {
+        content += "<tr>";
+        row.forEach(function(cell) {
+            content += "<td>" + cell + "</td>" ;
+        });
+        content += "</tr>";
+    });
+    document.getElementById("1").innerHTML = content;
+}
