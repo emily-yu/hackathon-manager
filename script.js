369ba58a5a179bec9a2351d9aa922e62cfd6f7f7
ey1031 <eyudeveloper@gmail.com>
2017-12-14 21:01:15 -0800

    remove unused methods

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-var csv = "city,state,population,land area, seattle,WA,652405,83.9, new york,NY,8405837,302.6, boston,MA,645966,48.3, kansas city,MO,467007,315.0"
-
-function loadData() {
-	function csvToArray (csv) {
-	    rows  = csv.split("\n");
-	    return rows.map(function (row) {
-	    	return row.split(",");
-	    });
-	};
-
-	// Hard-coded for brevity, but you can set this variable with FileReader
-	var csv = CSVParse("data.csv")
-
-	var array = csvToArray(csv);
-	return array;
-}
-
-function createTable2() {
-    var array = [["god bless",2,3,4],[4,5,6,4],[7,8,9,4]];
-    // var array = loadData();
-    var content = "";
-    array.forEach(function(row) {
-        content += "<tr>";
-        row.forEach(function(cell) {
-            content += "<td>" + cell + "</td>" ;
-        });
-        content += "</tr>";
-    });
-    document.getElementById("1").innerHTML = content;
-    document.getElementById("2").innerHTML = content;
-}
-
 function createNTable(array) {
     var content = "";
     array.forEach(function(row) {
@@ -49,10 +17,10 @@ function clean(obj) {
     }
   }
 }
-function deleteByValue(val, fruits) {
-    for(var f in fruits) {
-        if(fruits[f] == val) {
-            delete fruits[f];
+function deleteByValue(val, inputs) {
+    for(var f in inputs) {
+        if(inputs[f] == val) {
+            delete inputs[f];
         }
     }
 }
