abcf6d5ed52ec07b81ecd099a910a8c0ecfeeef1
ey1031 <eyudeveloper@gmail.com>
2017-12-22 23:38:58 -0800

    fixing header on sponsors and adding preview imgs

--- global.js
+++ global.js
@@ -1,5 +1,5 @@
 // var ngrok = 'https://pacific-escarpment-47591.herokuapp.com/'
-var ngrok = 'http://5c66e776.ngrok.io/'
+var ngrok = 'http://f791f916.ngrok.io/'
 
 function getNgrok() {
 	return ngrok;
